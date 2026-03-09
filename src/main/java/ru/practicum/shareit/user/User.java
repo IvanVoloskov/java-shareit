abe9a895ff73159e7c357059625e22cbab062016
Voloskov Ivan <voloskov.ivan03@mail.ru>
2026-03-09 22:50:00 +0300

    Исправить правки от ревьюера

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.user;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
